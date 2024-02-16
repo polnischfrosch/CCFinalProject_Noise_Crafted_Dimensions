503ec35fd898c08c5357f4ebc0c451ba75dce8d6
polnischfrosch <wagner.aaron.1997@gmail.com>
2024-02-16 16:26:28 +0100

    moved to different repositiory

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,15 @@ import { resolve } from 'path'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  base: "/CCFP_Noise_Crafted_Dimensions/",
+  base: "/CCFinalProject_Noise_Crafted_Dimensions/",
   build: {
     rollupOptions: {
       input: {
         main: resolve(__dirname, 'index.html'),
-        app: resolve(__dirname, 'app/index.html'),
+        app: resolve(__dirname, ' app.html'),
         howto: resolve(__dirname, 'howto/index.html'),
       },
     },
   },
 });
+
